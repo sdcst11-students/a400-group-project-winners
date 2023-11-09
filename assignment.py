a1d7c6b30a5fd7e00304d6876fdada1d4e9a3461
yuriif671 <yuriif767@deltalearns.ca>
2023-11-09 13:08:10 -0800

    formatting

--- assignment.py
+++ assignment.py
@@ -2,27 +2,14 @@
 #Team Winners
 import math
 
-
-
-def title():
-   
-    return None
-
-def instructions():
-    # Will display instructions
-    # input parameters: none needed
-    # output parameters: None
-    # Author:
-    # Modified:
-    return None
-
-#made by Yurii Fadieiev
-
+""""
 #Volume of a sphere
 #Surface area of a sphere
 #Factorial
 #Find fibonacci number
 #Interest calculations
+"""
+#made by Yurii Fadieiev
 
 def findSphereVolume():
     try: 
@@ -79,16 +66,7 @@ def findCompoundInterest():
 
     return round(p * (1 + (r / 100) / n)**(n * t), 2)
 
-def main():
-    """
-    main block of code that will run your program and control program flow
-    You will need to include a while loop to keep repeating the commands until
-    the user chooses to exit
-    """
-    title()
-    while True:
-        # keep giving options to choose menu options until they choose to exit
-        pass
+
 
 def Basmath():
 
@@ -188,10 +166,27 @@ Basmath()
 
 
 
+def title():
+    return None
 
+def instructions():
+    # Will display instructions
+    # input parameters: none needed
+    # output parameters: None
+    # Author:
+    # Modified:
+    return None
 
-
+def main():
+    """
+    main block of code that will run your program and control program flow
+    You will need to include a while loop to keep repeating the commands until
+    the user chooses to exit
+    """
+    title()
+    while True:
+        # keep giving options to choose menu options until they choose to exit
+        pass
 
 if __name__ == "__main__":
-    print(findCompoundInterest())
-    #main()
+    main()
