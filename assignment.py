fe34625678e2f56ffc84e3fcbae874722866d571
yuriif671 <yuriif767@deltalearns.ca>
2023-11-14 12:32:31 -0800

    did Yurii's work

--- assignment.py
+++ assignment.py
@@ -12,72 +12,77 @@ import math
 #made by Yurii Fadieiev
 
 def findSphereVolume():
-    try: 
-        r = float(input("Radius of the sphere: "))
-        return round(4.0/3.0 * math.pi * r**3, 2)
-    except Exception as e:
-        return("Error. Bad input.")
+    while True: 
+        try: 
+            r = float(input("Radius of the sphere: "))
+            print(f"The volume of the sphere is: {round(4.0/3.0 * math.pi * r**3, 2)}") 
+            break
+        except Exception as e:
+            print("Error. Bad input.")
 
 
 def findSphereSurfaceArea():
-    try:
-        r = float(input("Radius of the sphere: "))
-        return round(4 * math.pi * r**2, 2)
-    except Exception as e:
-        return("Error. Bad input.")
+    while True:
+        try:
+            r = float(input("Radius of the sphere: "))
+            print(f"The surface area of the sphere is: {round(4 * math.pi * r**2, 2)}")
+            break
+        except Exception as e:
+            print("Error. Bad input.")
 
 def findFactorial():
-    try:
-        n = int(input("Number to find the factorial of: "))
-        f = 1
-        for i in range(1, n+1):
-            f = f * i
-        return f
-    except Exception as e:
-        return("Error. Bad input.")
+    while True:
+        try:
+            n = int(input("Number to find the factorial of: "))
+            f = 1
+            for i in range(1, n+1):
+                f = f * i
+            print(f"The factorial is: {f}")
+            break
+        except Exception as e:
+            print("Error. Bad input.")
 
 #print(', '.join(map(str, findFibonacciSequence()))) to be used for the below
 def findFibonacciSequence():
-    n = int(input("Enter the number of factors: "))
+    while True:
+        try:
+            n = int(input("Enter the number to find the fibonacci sequence of: "))
 
-    out = []
+            out = []
 
-    a = 0
-    b = 1
-    sum = a + b
+            a = 0
+            b = 1
+            sum = a + b
 
-    #count = 1
+            #count = 1
 
-    while (a <= n):
-    	out.append(a)
+            while (a <= n):
+            	out.append(a)
 
-    	a = b
-    	b = sum
-    	sum = a + b
+            	a = b
+            	b = sum
+            	sum = a + b
 
-    return out
+            print(f"The fibonacci sequence is: {', '.join(map(str, out))}")
+            break
+        except Exception as e:
+            print("Error. Bad input.")
 
 #P(1 + r/n)nt
 def findCompoundInterest():
-    p = float(input("Principal: "))
-    r = float(input("Rate: "))
-    n = float(input("Compound periods per year: "))
-    t = float(input("Years: "))
-
-    return round(p * (1 + (r / 100) / n)**(n * t), 2)
-
-def main():
-    """
-    main block of code that will run your program and control program flow
-    You will need to include a while loop to keep repeating the commands until
-    the user chooses to exit
-    """
-    title()
     while True:
-        input("placehold")
-        Basmath()
-        pass
+        try:
+            p = float(input("Principal: "))
+            r = float(input("Rate: "))
+            n = float(input("Compound periods per year: "))
+            t = float(input("Years: "))
 
+            print(f"The interest is: {round(p * (1 + (r / 100) / n)**(n * t), 2)}")
+            break
+        except Exception as e:
+            print("Error. Bad input.")
+
+#the following is by John
 def Basmath():
 
     
@@ -220,6 +225,13 @@ def main():
     the user chooses to exit
     """
     title()
+
+    #test of Yurii's programs
+    findCompoundInterest()
+    findFactorial()
+    findFibonacciSequence()
+    findSphereSurfaceArea()
+    findSphereVolume()
     while True:
         # keep giving options to choose menu options until they choose to exit
         pass
