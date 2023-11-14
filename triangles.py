0af193f95e79b3cc5cf1d1253b29b0174d7c6e32
tylerbohl <144383251+tylerbohl@users.noreply.github.com>
2023-11-14 12:40:25 -0800

    nov14

---
+++ triangles.py
@@ -0,0 +1,50 @@
+#Soh Cah Toa
+# if Q is True y is numerator if Q is False y is denominator
+
+import math
+
+def Cos(d,y,Q):
+    try:
+        a = math.cos(d*math.pi/180)
+        print(a)
+    except:
+        answer = f"Error\n{d}° doesnt have a cosine"
+        return answer
+    if Q == True:
+        answer = y/a
+        answer = round(answer,3)
+    if Q == False:
+        answer = y*a
+        answer = round(answer,3)
+    return answer
+    
+def Tan(d,y,Q):
+    try:
+        a = math.tan(d*math.pi/180)
+    except:
+        answer = f"Error\n{d}° doesnt have a tangent"
+        return answer
+    if Q == True:
+        answer = y/a
+        answer = round(answer,3)
+    if Q == False:
+        answer = y*a
+        answer = round(answer, 3)
+    return answer
+
+def Sin(d,y,Q):
+    try:
+        a = math.sin(d*math.pi/180)
+    except:
+        answer = f"Error\n{d}° doesnt have a sine"
+        return answer
+    if Q == True:
+        answer = y/a
+        answer = round(answer,3)
+    if Q == False:
+        answer = y*a
+        answer = round(answer, 3)
+    return answer
+
+# cos = y/x
+print(Sin(60,24,True))
